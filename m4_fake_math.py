060ea62afde133cb5912d2f37229a6e91c8ca142
ivanartemiev <art.lx.ru@gmail.com>
2024-12-18 13:35:04 +0300

    Add files via upload

---
+++ m4_fake_math.py
@@ -0,0 +1,6 @@
+def divide(first, second):
+    if second == 0:
+        div_res = 'Ошибка!'
+    else:
+        div_res =  first / second
+    return div_res
---
+++ m4_true_math.py
@@ -0,0 +1,7 @@
+from math import inf
+def divide(first, second):
+    if second:
+        div_res = first/ second
+    else:
+        return inf
+    return div_res
